9b9d22c14aa2472358c33f5ea86f78ec11ef0379
Jero2244 <jerojuarezpacheco@gmail.com>
2022-02-22 21:46:25 -0300

    css for left side finished and some js done

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-let num;
-let percentage;
+// let num;
+// let percentage;
 
-function getVal(){
-    const number = document.querySelector('input').value;
-    console.log(number)
-    num = Number(number)
+// function getVal(){
+//     const number = document.querySelector('input').value;
+//     console.log(number)
+//     num = Number(number)
+// }
+
+// function sum(){
+//     let result = num + num
+//     console.log(result)
+// }
+// function percentages(){
+//     const btn = document.querySelector('.button-1')
+//     const btncontent = btn.textContent;
+//     percentage = Number(btncontent)
+//     console.log(percentage)
+
+// }
+
+// const btn = document.querySelectorAll('button');
+// btn.onClick = () => console.log('Clicked')
+
+
+if (document.body.addEventListener){
+    document.body.addEventListener('click',yourHandler,false);
+}
+else{
+    document.body.attachEvent('onclick',yourHandler);//for IE
 }
 
-function sum(){
-    let result = num + num
-    console.log(result)
+function yourHandler(e){
+    e = e || window.event;
+    const target = e.target || e.srcElement;
+    if (target.className.match('button'))
+    {
+        show(target)
+        target.classList.add('active');
+        
+    }
 }
-function percentages(){
-    const btn = document.querySelector('.button-1')
-    const btncontent = btn.textContent;
-    percentage = Number(btncontent)
-    console.log(percentage)
 
+function show(btn){
+    var btns = document.querySelectorAll('button');
+    btns.forEach(function(btn){
+      btn.classList.remove('active');
+    });
+    // btn.classList.add('active');
 }
-
-const btn = document.querySelectorAll('button');
-btn.onClick = () => console.log('Clicked')
-
